6c7288afb71e852fb2809ca00974b5aece408c11
AbdulhakimKaya <abdulhakimkaya2199@gmail.com>
2024-02-18 17:32:52 +0300

    CQRS ve MediatR İmplementasyonu

---
+++ Domain/Entities/Brand.cs
@@ -0,0 +1,17 @@
+using Core.Persistence.Repositories;
+
+namespace Domain.Entities;
+
+public class Brand : Entity<Guid>
+{
+    public string Name { get; set; }
+
+    public Brand()
+    {
+    }
+
+    public Brand(Guid id, string name) : base(id)
+    {
+        Name = name;
+    }
+}
---
+++ WebAPI/Controllers/BrandsController.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.Features.Brands.Commands.Create;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrandsController : BaseController
+    {
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] CreateBrandCommand createBrandCommand)
+        {
+            CreatedBrandResponse response = await Mediator!.Send(createBrandCommand);
+            return Ok(response);
+        }
+    }
+}
