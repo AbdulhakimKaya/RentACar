a564896223545af044e93bae50d63225db9c0c9e
AbdulhakimKaya <abdulhakimkaya2199@gmail.com>
2024-02-19 13:04:10 +0300

    Persistence Layer İmplementasyonu

--- Domain/obj/Debug/net7.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net7.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b02c73b51f549fb3b710eb4895b2c47be6c80738")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
