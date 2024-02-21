3a45321a0175ded9b527f55e41da5bea9d2b18ce
AbdulhakimKaya <abdulhakimkaya2199@gmail.com>
2024-02-21 06:27:41 +0300

    İş Kuralları ve SubClass Type IoC İmplementasyonu

--- Application/Features/Brands/Commands/Create/CreateBrandCommand.cs
+++ Application/Features/Brands/Commands/Create/CreateBrandCommand.cs
@@ -1,3 +1,4 @@
+using Application.Features.Brands.Rules;
 using Application.Services.Repositories;
 using AutoMapper;
 using Domain.Entities;
@@ -12,16 +13,20 @@ public class CreateBrandCommand : IRequest<CreatedBrandResponse>
     public class CreateBrandCommandHandler : IRequestHandler<CreateBrandCommand, CreatedBrandResponse>
     {
         private readonly IBrandRepository _brandRepository;
+        private readonly BrandBusinessRules _brandBusinessRules;
         private readonly IMapper _mapper;
 
-        public CreateBrandCommandHandler(IBrandRepository brandRepository, IMapper mapper)
+        public CreateBrandCommandHandler(IBrandRepository brandRepository, BrandBusinessRules brandBusinessRules, IMapper mapper)
         {
             _brandRepository = brandRepository;
+            _brandBusinessRules = brandBusinessRules;
             _mapper = mapper;
         }
 
         public async Task<CreatedBrandResponse> Handle(CreateBrandCommand request, CancellationToken cancellationToken)
         {
+            await _brandBusinessRules.BrandNameCannotBeDuplicatedWhenInserted(request.Name);
+            
             Brand brand = _mapper.Map<Brand>(request);
             brand.Id = Guid.NewGuid();
 
--- Application/obj/Debug/net7.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net7.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8726ea6a4a75a319de886d293984be5713c3b43")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b7aa1e96228d885a51b3b887dafd50584ebc440")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
